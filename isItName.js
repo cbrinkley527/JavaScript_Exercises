2cc71a09d8fde91effdfe0b339ff734079b571bb
Celeste Brinkley <brinkleyc.527@gmail.com>
2020-10-08 15:31:14 -0500

    finished assignments

---
+++ isItName.js
@@ -0,0 +1,18 @@
+// Write a function called "isItName" that accepts one argument
+// of type String named "name" and returns either "Ola Universe" if the name
+// is empty string eg. "" or not provided at all or "Ola <name>"
+
+
+// *** YOUR CODE HERE *** //
+
+function isItName(name) {
+  if (name) {
+    return "Ola" + (name)
+  }
+  else (null) {
+    return "Ola Universe";
+  }
+
+}
+
+module.exports = isItName;
